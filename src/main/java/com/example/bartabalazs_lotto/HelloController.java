e5f57d3f655a9661be5732c49658dd70e0ac2e10
Barta Balázs <barta.balazs00@gmail.com>
2021-12-01 16:52:01 +0100

    sorsol

--- src/main/java/com/example/bartabalazs_lotto/HelloController.java
+++ src/main/java/com/example/bartabalazs_lotto/HelloController.java
@@ -2,18 +2,44 @@ package com.example.bartabalazs_lotto;
 
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+
+import java.time.Duration;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
+import java.util.concurrent.TimeUnit;
 
 public class HelloController {
     @FXML
     private Label welcomeText;
-
     @FXML
-    protected void onHelloButtonClick() {
-        welcomeText.setText("Welcome to JavaFX Application!");
+    private Button btn_sorsol;
+    private List<Integer> voltSzamok;
+    private Random random;
+    public Label szamEppen;
+    public Label szamok;
+    private String szamokVoltak;
+    @FXML
+    public void initialize(){
+        random = new Random();
+        szamokVoltak = "";
+        voltSzamok = new ArrayList<>();
     }
 
-    public void sorsol(ActionEvent actionEvent) {
-
+    public void sorsol(ActionEvent actionEvent) throws InterruptedException {
+        for (int i = 0; i < 5; i++) {
+            int szam = random.nextInt(90)+1;
+            if(!voltSzamok.contains(szam)){
+                TimeUnit.SECONDS.sleep(2);
+                szamEppen.setText(""+szam);
+                voltSzamok.add(szam);
+                szamokVoltak+= " "+szam;
+                szamok.setText(szamokVoltak);
+            } else {
+                i--;
+            }
+        }
     }
 }
